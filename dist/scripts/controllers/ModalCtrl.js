d75f50df08b065ceacb410850879956bf8f4a8b6
Virginia <vcooper_us@yahoo.com>
2016-08-03 21:37:52 -0700

    first attempt at creating chat rooms.

---
+++ dist/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+    function ModalCtrl($uibModal) {
+        this.newChatRoom = function() {
+            
+        }
+    }
+    angular
+        .module('blocChat')
+        .controller('ModalCtrl', ['Modal', ModalCtrl]);
+})();
