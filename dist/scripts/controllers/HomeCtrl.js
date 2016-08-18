95e9865fa46ae9d1f0e986cdad4b26ce88252284
Virginia <vcooper_us@yahoo.com>
2016-08-18 16:01:00 -0700

    Successfully implemented cancel function.

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -2,25 +2,22 @@
     function HomeCtrl(Room, $uibModal,$scope) {
         this.chatRooms = Room.all;
         
+        // preserve the value of "this"
+        var Home = this;
         this.openModal = function() {
-            this.modalInstance = $uibModal.open({
+            // inside here, "this" refers to the "openModal" function, not the HomeCtrl
+            Home.modalInstance = $uibModal.open({
 //               template: "<h1>Hello world</h1>",
                templateUrl: '/templates/modal.html',
-                controller: 'ModalCtrl'
+                controller: "ModalCtrl"
+//                controller: function($scope, $uibModalInstance) {
+//                    $scope.closeModal = function() {
+//                        alert("Should close the modal!");
+//                    }
+//                }
                        });
         }
         
-        this.closeModal = function() {
-            this.modalInstance = $uibModal.close({
-                
-            });
-        }
-        
-        this.submitModal = function() {
-            this.modalInstance = $uibModal.submit({
-                
-            });
-        }
     }
     
     angular
