c5d6145e5b00379cb536e8ad432ecb353dc974f7
Virginia <vcooper_us@yahoo.com>
2016-08-21 20:34:26 -0700

    fixed cancel function.

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -4,25 +4,35 @@
         
         // preserve the value of "this"
         var Home = this;
+        
+        Home.roomService = Room;
+        
         this.openModal = function() {
             // inside here, "this" refers to the "openModal" function, not the HomeCtrl
             Home.modalInstance = $uibModal.open({
 //               template: "<h1>Hello world</h1>",
                templateUrl: '/templates/modal.html',
                 controller: "ModalCtrl"
+                
 //                controller: function($scope, $uibModalInstance) {
-//                    $scope.closeModal = function() {
-//                        alert("Should close the modal!");
+//                    $scope.newRoom = {name: ''};
+//                    
+        /*              $scope.create = function() {
+                        $uibModalInstance.close($scope.newRoom);
+                        }*/
 //                    }
 //                }
                        });
-        }
-       
-         modalInstance.result.then(function (selectedItems) {
+            
+            modalInstance.result.then(function (selectedItem) {
       this.selected = selectedItem;
+                Home.roomService.addRoom(selectedItem);
     }, function () {
       $log.info('Modal dismissed at: ' + new Date());
     });
+        }
+       
+         
         
     }
     angular
--- dist/scripts/controllers/ModalCtrl.js
+++ dist/scripts/controllers/ModalCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-<<<<<<< HEAD
+
     function ModalCtrl($uibModalInstance, $scope, Room) {
         
           $scope.closeModal = function(){
@@ -11,27 +11,8 @@
               // Call your Modal.Create("room_name") method to create the new room.
 //              $uibModalInstance.create()
           }
-=======
-    function ModalCtrl($uibModalInstance) {
-        
-            this.closeModal = function() {
-                $uibModalInstance.dismiss();
-                console.log("Call closeModal");
-            }
-            
-            this.submitModal = function() {
-                $uibModalInstance.close(this.newRoom.name);
-            }
-       /* this.create = function(rooms) {
-            return rooms.$add(room);
-        },
-            this.get = function (roomId) {
-                return $firebase(ref.child('messages').child(messageId)).$asObject();
-            },
-                this.delete = function (room) {
-                    return rooms$remove(room);
-                }*/
->>>>>>> 1405ce032a0db900531daf3e3a786b5faf51e2c8
+
+   
     };
     
     
